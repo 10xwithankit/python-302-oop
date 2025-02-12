380a35cc5242940d3b1d45f672d51bd4a4962ee2
Ankit Maheshwari <maheshwari.ankit@gmail.com>
2025-02-12 20:03:09 +0530

    Code for Chap2

--- chap2_solution.py
+++ chap2_solution.py
@@ -2,21 +2,26 @@
 #   - create instance variables - name, power_level
 #   - create method(s):
 #       - attack: prints "{name} attacks with power level {power_level}!"
-
-
 class SuperHero:
+    name: str
+    power_level: int
+
     def __init__(self, name, power_level):
         self.name = name
         self.power_level = power_level
 
     def attack(self):
-        print(f"{self.name} is on power level {self.power_level}!")
+        print(f"{self.name} attacks with power level {self.power_level}!")
 
 
-# Creating hero objects
+# Instantiate 2 hero objects
 ww = SuperHero("Wonder Woman", 100)
 bm = SuperHero("Batman", 90)
 
-# Use hero abilities
+# Print hero names and their power_level (for each objects)
+print(ww.name, ww.power_level)
+print(bm.name, bm.power_level)
+
+# Call attack method for each obejct
 ww.attack()
 bm.attack()
