2a04c42bfcad1a0cdce710199d1b2dee2fb3565a
Anit-1to10x <anit.c@1to10x.com>
2025-02-13 15:56:50 +0530

    Add files via upload

---
+++ chap3_3_solution.py
@@ -0,0 +1,10 @@
+# Define a function `greet` that prints a greeting message
+def greet():
+    print("Hello!")
+
+# Assign the function `greet` to a new variable `f`
+# `f` now points to the same function as `greet`
+f = greet  
+
+# Call the function using the new variable `f`
+f()  # Output: Hello!
