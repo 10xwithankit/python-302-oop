9d3cce42ce0de9c42bbb52222e67aee2d53abf87
Anit-1to10x <anit.c@1to10x.com>
2025-02-14 19:05:57 +0530

    Update chap5_2.py

--- code/chap_5/chap5_2.py
+++ code/chap_5/chap5_2.py
@@ -1,15 +1,21 @@
 # Define a class `Person`
 
-# Define the constructor method (__init__)
-# This method initializes the person's attributes
+    
+    # Define the constructor method (__init__)
+    # This method initializes the person's attributes
+    
+        
+        # Public attribute: Can be accessed directly
+        
+        
+        # Private attribute: Cannot be accessed directly outside the class
+        
 
-# Public attribute: Can be accessed directly
-
-# Private attribute: Cannot be accessed directly outside the class
-
-# Define a getter method `get_age`
-# This method provides controlled access to the private attribute `__age`
+    # Define a getter method using @property
+     
 
 # Create an instance of the `Person` class
 
-# Accessing the private attribute using the getter method
+
+# Accessing the private attribute using the getter
+
