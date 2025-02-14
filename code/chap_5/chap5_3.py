e54a85fccef259323b897d870b45a119b7233ca4
Anit-1to10x <anit.c@1to10x.com>
2025-02-14 19:03:04 +0530

    Update chap5_3.py

--- code/chap_5/chap5_3.py
+++ code/chap_5/chap5_3.py
@@ -1,30 +1,25 @@
 # Define a class `Person`
-# Define the constructor method (__init__)
-# This method initializes the person's attributes
-# Public attribute: Can be accessed directly
-
 class Person:
+    
+    # Define the constructor method (__init__)
+    # This method initializes the person's attributes
     def __init__(self, name, age):
-        self.name = name
-        self.__age = age  # Private Attribute
+        
+        # Public attribute: Can be accessed directly
+        self.name = name  
+        
+        # Private attribute: Cannot be accessed directly outside the class
+        self.__age = age  
+
+    # Define a getter method using @property
+    @property
+    def age(self):  
+        return self.__age  
+
+    # Define a setter method using @age.setter
+    
+        
+
+# Update age using the setter
 
 
-# Private attribute: Cannot be accessed directly outside the class
-
-# Define a setter method `set_age`
-# This method allows controlled modification of the private attribute `__age`
-
-# Check if the new age is a positive number
-# Update the age if the condition is met
-# Print an error message if invalid
-
-# Define a getter method `get_age`
-# This method provides controlled access to the private attribute `__age`
-
-# Create an instance of the `Person` class
-
-# Update age using the setter method
-
-# Retrieve the updated age using the getter method
-
-# Attempt to set an invalid age (negative number)
