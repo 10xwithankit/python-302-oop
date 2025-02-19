64410e27d8bbbd0f943380e3ba42edfe1e5d82e0
AnshulBhati1to10x <anshul.b@1to10x.com>
2025-02-19 19:31:59 +0530

    Update chap9_2.py

--- code/chap_9/chap9_2.py
+++ code/chap_9/chap9_2.py
@@ -1,10 +1,16 @@
 # Import the ABC module to define an abstract class
-
 # Define an abstract class `Payment`
 # Abstract classes cannot be instantiated and must be inherited by subclasses
-
 # Define an abstract method `process_payment`
 # This method must be implemented by any subclass
+
+from abc import ABC, abstractmethod
+
+class Payment(ABC):
+    @abstractmethod
+    def process_payment(self, amount):
+        pass
+
 
 # Define a subclass `CreditCardPayment` that inherits from `Payment`
 
