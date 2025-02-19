237306df5634b4f229da7f0905814a863b686211
AnshulBhati1to10x <anshul.b@1to10x.com>
2025-02-19 19:30:49 +0530

    Update chap9_2.py

--- code/chap_9/chap9_2.py
+++ code/chap_9/chap9_2.py
@@ -1,28 +1,28 @@
 # Import the ABC module to define an abstract class
+
 # Define an abstract class `Payment`
 # Abstract classes cannot be instantiated and must be inherited by subclasses
+
 # Define an abstract method `process_payment`
 # This method must be implemented by any subclass
-
-from abc import ABC, abstractmethod
-
-class Payment(ABC):
-    @abstractmethod
-    def process_payment(self, amount):
-        pass
-
-
 
 # Define a subclass `CreditCardPayment` that inherits from `Payment`
 
 # Implement the `process_payment` method
-# This method returns a message for processing a credit card payment
+# This method prints a message for processing a credit card payment
 
 # Define another subclass `PayPalPayment` that inherits from `Payment`
 
 # Implement the `process_payment` method
-# This method returns a message for processing a PayPal payment
+# This method prints a message for processing a PayPal payment
 
-# Create instances of `CreditCardPayment` and `PayPalPayment`
+# Define another subclass `BankTransferPayment` that inherits from `Payment`
 
-# Call the `process_payment` method for `CreditCardPayment` and `PayPalPayment`
+# Implement the `process_payment` method
+# This method prints a message for processing a bank transfer payment
+
+# Create an instance of `CreditCardPayment` and process a payment
+
+# Create an instance of `PayPalPayment` and process a payment
+
+# Create an instance of `BankTransferPayment` and process a payment
