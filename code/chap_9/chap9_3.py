65c0cf0524cd7b2eb53e27ced74b2cf32c93ac67
AnshulBhati1to10x <anshul.b@1to10x.com>
2025-02-19 19:22:50 +0530

    Update chap9_3.py

--- code/chap_9/chap9_3.py
+++ code/chap_9/chap9_3.py
@@ -1,14 +1,21 @@
+# Import the ABC module to define an abstract class
+
 # Define an abstract class `DataSource`
 # Abstract classes cannot be instantiated and must be inherited by subclasses
+
 # Define an abstract method `read_data`
 # This method must be implemented by any subclass
 
 # Define a subclass `CSVSource` that inherits from `DataSource`
 
 # Implement the `read_data` method
-# This method prints a message for reading data from a CSV file
+# This method returns a message for reading data from a CSV file
 
 # Define another subclass `SQLSource` that inherits from `DataSource`
 
 # Implement the `read_data` method
-# This method prints a message for reading data from an SQL
+# This method returns a message for reading data from an SQL database
+
+# Create instances of `CSVSource` and `SQLSource`
+
+# Call the `read_data` method for `CSVSource` and `SQLSource`
